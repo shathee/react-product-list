3e40cf841540da2b6f4f806c96f31785dc7c42bf
Baki <sat.sust@gmail.com>
2021-04-21 17:02:55 +0200

    data loading by import instead of fetching

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -7,6 +7,10 @@ import Product from './Product';
 import LoadingSpinner from './LoadingSpinner';
 import ProductListHeader from './ProductListHeader';
 import { FixedSizeList as List } from "react-window";
+import productData from '../assets/data/products.json';
+import shelfData from '../assets/data/shelfs.json';
+import groupData from '../assets/data/product_groups.json';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
 
 const ProductListDiv = styled.div`
@@ -18,35 +22,40 @@ const ProductListDiv = styled.div`
 function ProductList() {
   
   const products = useSelector( (state) => state.visible_products);
-  
   const dispatch = useDispatch();
-  const [prodactDataLoaded, setProdactDataLoaded] = useState(false);
-  const [shelfDataLoaded, setShelfDataLoaded] = useState(false);
-  const [groupDataLoaded, setGroupDataLoaded] = useState(false);
+  const [dataLoaded, setDataLoaded] = useState(false);
+  // const [prodactDataLoaded, setProdactDataLoaded] = useState(false);
+  // const [shelfDataLoaded, setShelfDataLoaded] = useState(false);
+  // const [groupDataLoaded, setGroupDataLoaded] = useState(false);
 
   useEffect(() => {
-    async function getProductsData() {
-      let response = await fetch('./data/products.json')
-      response = await response.json()
-      dispatch(setIntialProductList(response));
-      setProdactDataLoaded(true)
-    }
-    async function getShelfData() {
-      let response = await fetch('./data/shelfs.json')
-      response = await response.json()
-      dispatch(setProductShelfs(response));
-      setShelfDataLoaded(true)
-    }
-    async function getGroupData() {
-      let response = await fetch('./data/product_groups.json')
-      response = await response.json()
-      dispatch(setProductGroups(response));
-      setGroupDataLoaded(true)
-    }
+    // async function getProductsData() {
+    //   let response = await fetch('./data/products.json')
+    //   response = await response.json()
+    //   dispatch(setIntialProductList(response));
+    //   setProdactDataLoaded(true)
+    // }
+    // async function getShelfData() {
+    //   let response = await fetch('./data/shelfs.json')
+    //   response = await response.json()
+    //   dispatch(setProductShelfs(response));
+    //   setShelfDataLoaded(true)
+    // }
+    // async function getGroupData() {
+    //   let response = await fetch('./data/product_groups.json')
+    //   response = await response.json()
+    //   dispatch(setProductGroups(response));
+    //   setGroupDataLoaded(true)
+    // }
 
-    getProductsData();
-    getShelfData();
-    getGroupData();
+    // getProductsData();
+    // getShelfData();
+    // getGroupData();
+    dispatch(setIntialProductList(productData));
+    dispatch(setProductShelfs(shelfData));
+    dispatch(setProductGroups(groupData));
+    setDataLoaded(true)
+
   }, [])
 
 
@@ -55,15 +64,21 @@ function ProductList() {
   )
 
   let contents;
-  if (prodactDataLoaded & shelfDataLoaded & groupDataLoaded) {
-    contents = <List
-              width={1340}
-              height={products.length}
-              itemCount={products.length}
-              itemSize={35}
-            >
-              {Row}
-            </List>
+  // if (prodactDataLoaded & shelfDataLoaded & groupDataLoaded) {
+  if (dataLoaded) {
+    contents = <AutoSizer>
+      {( {height, width}) => (
+        <List
+          width={1340}
+          height={height}
+          itemCount={products.length}
+          itemSize={35}
+        >
+          {Row}
+        </List> 
+      )}
+            
+            </AutoSizer>
   }else{
     contents = <LoadingSpinner />;
   }
